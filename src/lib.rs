839c6c38674a6cf194f960e74ad6f651b4b97c37
MaximumOverflow <maximumoverflow904@gmail.com>
2022-12-02 18:58:42 +0100

    Implemented Danbooru support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
 pub mod search;
+pub mod danbooru;
+
+use chrono::Utc;
+use std::any::Any;
+use serde_derive::Deserialize;
+
 pub use search::*;
+pub type Timestamp = chrono::DateTime<Utc>;
 
-pub trait Post {
+pub trait Post: Any + internal_traits::Search {
+	fn md5(&self) -> &str;
+	fn score(&self) -> isize;
+	fn rating(&self) -> Rating;
+	fn resource_url(&self) -> &str;
+	fn tags<'l>(&'l self) -> Box<dyn Iterator<Item = &'l str> + 'l>;
+}
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Deserialize)]
+pub enum Rating {
+	#[serde(alias = "g")]
+	General,
+	#[serde(alias = "s")]
+	Sensitive,
+	#[serde(alias = "q")]
+	Questionable,
+	#[serde(alias = "e")]
+	Explicit,
 }
--- src/search.rs
+++ src/search.rs
@@ -10,18 +10,25 @@ pub enum Source {
 	Danbooru,
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug)]
 pub enum Error {
-	UnimplementedSource
+	EmptyResponse,
+	UnimplementedSource,
+	
+	Generic(String),
+	InvalidResponse(Vec<u8>),
+	RequestFailed(reqwest::Error),
+	JsonDeserializationFailed(serde_json::Error),
 }
 
 #[derive(Debug, Default)]
 pub struct SearchBuilder<'l> {
+	limit: usize,
 	include: HashSet<&'l str>,
 	exclude: HashSet<&'l str>,
 }
 
-impl <'l> SearchBuilder<'l> {
+impl<'l> SearchBuilder<'l> {
 	pub fn include_tag(&mut self, tag: &'l str) -> &mut Self {
 		self.exclude.remove(tag);
 		self.include.insert(tag);
@@ -33,16 +40,16 @@ impl <'l> SearchBuilder<'l> {
 		self.exclude.insert(tag);
 		self
 	}
-	
-	pub fn include_tags(&mut self, tags: impl Iterator<Item=&'l str>) -> &mut Self {
+
+	pub fn include_tags(&mut self, tags: impl Iterator<Item = &'l str>) -> &mut Self {
 		for tag in tags {
 			self.include_tag(tag);
 		}
-		
+
 		self
 	}
 
-	pub fn exclude_tags(&mut self, tags: impl Iterator<Item=&'l str>) -> &mut Self {
+	pub fn exclude_tags(&mut self, tags: impl Iterator<Item = &'l str>) -> &mut Self {
 		for tag in tags {
 			self.exclude_tag(tag);
 		}
@@ -50,22 +57,72 @@ impl <'l> SearchBuilder<'l> {
 		self
 	}
 	
-	pub fn search<'p>(self, source: Source) -> Result<Box<dyn Iterator<Item=&'p dyn Post>>, Error> {
+	pub fn limit(&mut self, limit: usize) -> &mut Self {
+		self.limit = limit;
+		self
+	}
+
+	pub fn search_dyn<'p>(self, source: Source) -> Result<Box<dyn Iterator<Item = &'p dyn Post> + 'p>, Error> {
 		use Error::*;
-		
+		use Source::*;
+		use crate::search::internal_traits::Search;
+
+		let tags = self.make_tags();
+
 		match source {
-			_ => Err(UnimplementedSource)
+			Danbooru => {
+				let posts = crate::danbooru::Post::search(tags, self.limit)?;
+				let iter = PostIterator::new(posts);
+				Ok(Box::new(iter))
+			}
+
+			_ => Err(UnimplementedSource),
+		}
+	}
+
+	pub fn search<T: Post>(self) -> Result<Vec<T>, Error> {
+		let tags = self.make_tags();
+		T::search(tags, self.limit)
+	}
+
+	fn make_tags(&self) -> String {
+		let mut tags = String::new();
+		for tag in self.include.iter() {
+			if tags.len() != 0 {
+				tags.push('+');
+			}
+			tags.extend(transform_tag(tag));
+		}
+
+		for tag in self.exclude.iter() {
+			if tags.len() != 0 {
+				tags.push('+');
+			}
+			tags.push('-');
+			tags.extend(transform_tag(tag));
+		}
+
+		tags
+	}
+}
+
+struct PostIterator<'l, T: Post> {
+	index: usize,
+	posts: Vec<T>,
+	ph: PhantomData<&'l ()>,
+}
+
+impl<T: Post> PostIterator<'_, T> {
+	fn new(posts: Vec<T>) -> Self {
+		Self {
+			posts,
+			index: 0,
+			ph: Default::default(),
 		}
 	}
 }
 
-struct PostIterator<'l, P: Post> {
-	index: usize,
-	posts: Vec<P>,
-	ph: PhantomData<&'l()>,
-}
-
-impl <'l, P: 'l + Post> Iterator for PostIterator<'l, P> {
+impl<'l, T: 'l + Post> Iterator for PostIterator<'l, T> {
 	type Item = &'l dyn Post;
 
 	fn next(&mut self) -> Option<Self::Item> {
@@ -77,4 +134,28 @@ impl <'l, P: 'l + Post> Iterator for PostIterator<'l, P> {
 			Some(post)
 		}
 	}
+}
+
+pub(crate) mod internal_traits {
+	use crate::{Error, SearchBuilder};
+
+	pub trait SearchPriv {}
+	impl SearchPriv for SearchBuilder<'_> {}
+
+	pub trait Search {
+		fn search(params: String, limit: usize) -> Result<Vec<Self>, Error>
+		where
+			Self: Sized;
+	}
+}
+
+fn transform_tag(tag: &str) -> impl Iterator<Item=char> + '_ {
+	tag.chars()
+		.map(|mut c| {
+			if c == ' ' {
+				c = '_'
+			}
+			c.to_lowercase()
+		})
+		.flatten()
 }
