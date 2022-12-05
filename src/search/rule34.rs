024d9f41fab22fd66454704492bc22c704abb726
MaximumOverflow <maximumoverflow904@gmail.com>
2022-12-05 23:17:10 +0100

    Temporary fix to Rule34 returning mangled posts sometimes

--- src/search/rule34.rs
+++ src/search/rule34.rs
@@ -1,5 +1,6 @@
 use crate::search::{Search, SearchBuilder, SearchResult, Error, SearchAsync, SearchFuture};
 use crate::rule34::{Post, Rule34};
+use serde_json::Value;
 
 impl Search for Rule34 {
 	type Post = Post;
@@ -37,19 +38,52 @@ impl SearchAsync for Rule34 {
 
 fn deserialize(byte_vec: Vec<u8>) -> Result<Vec<Post>, Error> {
 	use Error::*;
+	use crate::data::Rating::*;
 	let bytes = byte_vec.as_slice();
-	match bytes {
-		//['[', .., ']'] | ['[', .., ']', '\n']
-		[0x5B, .., 0x5D] | [0x5B, .., 0x5D, 0x0A] => {
-			let posts = serde_json::from_slice::<Vec<Post>>(bytes)
-				.map_err(|err| Error::from((bytes, err)))?;
+
+	let data = serde_json::from_slice::<Value>(bytes)
+		.map_err(|err| Error::from((bytes, err)))?;
+	
+	match data {
+		Value::Array(values) => {
+			let posts = values.iter().filter_map(|value| {
+				let object = value.as_object()?;
+				let post = Post {
+					id: object.get("id")?.as_u64()? as usize,
+					directory: object.get("directory")?.as_u64()? as usize,
+					hash: object.get("hash")?.as_str()?.to_owned(),
+					width: object.get("width")?.as_u64()? as usize,
+					height: object.get("height")?.as_u64()? as usize,
+					image: object.get("image")?.as_str()?.to_owned(),
+					change: object.get("change")?.as_u64()? as usize,
+					owner: object.get("owner")?.as_str()?.to_owned(),
+					parent_id: object.get("parent_id")?.as_u64()? as usize,
+					rating: match object.get("rating")?.as_str()? {
+						"safe" => Some(Safe),
+						"general" => Some(General),
+						"explicit" => Some(Explicit),
+						"sensitive" => Some(Sensitive),
+						"questionable" => Some(Questionable),
+						_ => None,
+					}?,
+					sample: object.get("sample")?.as_u64()? as usize,
+					sample_width: object.get("sample_width")?.as_u64()? as usize,
+					sample_height: object.get("sample_height")?.as_u64()? as usize,
+					score: object.get("score")?.as_i64()? as isize,
+					tags: object.get("tags")?.as_str()?.to_owned(),
+					file_url: object.get("file_url")?.as_str()?.to_owned(),
+					sample_url: object.get("sample_url")?.as_str()?.to_owned(),
+					preview_url: object.get("preview_url")?.as_str()?.to_owned(),
+				};
+				Some(post)
+			}).collect();
 			
 			Ok(posts)
-		}
-
+		},
+		
 		_ => match String::from_utf8(byte_vec) {
 			Ok(text) => Err(InvalidResponse(text)),
 			Err(error) => Err(InvalidResponseBytes(error.into_bytes())),
-		},
+		}
 	}
 }
